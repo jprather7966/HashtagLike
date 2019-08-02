ea1bd0babba0a8b63c3b1895ef8d018bacb363e9
Jared Prather <jprather7966@yahoo.com>
2019-08-02 08:12:57 -0500

    Bot can now login into instagram

--- program.py
+++ program.py
@@ -2,8 +2,9 @@
 from selenium import webdriver
 # Allows us to plug in keys on the web driver
 from selenium.webdriver.common.keys import Keys
-# Allows us to check if the webpage has loaded
-from selenium.webdriver.support import expected_conditions 
+#allows us to delay bot until webpage is loaded
+import time
+
 
 class InstaBot:
     def __init__(self,username,password):
@@ -12,11 +13,23 @@ class InstaBot:
         self.bot = webdriver.Firefox()
     def login (self):
         bot = self.bot
-        bot.get('https://www.instagram.com/')
-        bot.implicitly_wait(5)
+        bot.get("https://www.instagram.com/accounts/login/")
+        time.sleep(5)
+        # Searches place to put in login and password
+        UsernameLogin = bot.find_element_by_name("username")
+        PasswordLogin = bot.find_element_by_name("password")
+        #Clears login in case username and password is already there
+        UsernameLogin.clear()
+        PasswordLogin.clear()
+        #Puts in username and password then hits enter to login
+        UsernameLogin.send_keys(self.username)
+        PasswordLogin.send_keys(self.password)
+        PasswordLogin.send_keys(Keys.RETURN)
+        time.sleep(5)
+
 
 username = open("username.txt","r")
 password = open("password.txt","r")
 
-ed = InstaBot(username,password)
+ed = InstaBot(username.read(),password.read())
 ed.login()
